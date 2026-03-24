0f8eb8dc02e765d2c15de2c55f4ad1a8af886930
Sayed Hussain <husain187@hotmail.com>
2026-03-24 12:40:09 +0300

    added required microservices

---
+++ Microservices/discovery/src/test/java/smahfood/neo4flix/discovery/smoke/DiscoverySmokeTest.java
@@ -0,0 +1,13 @@
+package smahfood.neo4flix.discovery.smoke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DiscoverySmokeTest {
+
+    @Test
+    void smoke() {
+        assertTrue(true);
+    }
+}
---
+++ Microservices/gateway/src/test/java/smahfood/ecommerce/gateway/smoke/GatewaySmokeTest.java
@@ -0,0 +1,13 @@
+package smahfood.ecommerce.gateway.smoke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GatewaySmokeTest {
+
+    @Test
+    void smoke() {
+        assertTrue(true);
+    }
+}
